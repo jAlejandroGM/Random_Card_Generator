1eff9e600fa50dfaa1744059c5853c3197f0ffe4
Alejandro Guzmán <j.alejandro.guzman.m@gmail.com>
2025-04-15 04:38:30 +0000

    proyecto terminado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,31 @@
-import "bootstrap";
 import "./style.css";
 
+let randomNumber = () => {
+  let num = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"];
+  let iNumbers = Math.floor(Math.random()*num.length);
+  return num[iNumbers];
+}
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let randomSuit = () => {
+  let suit = ["♠","♣","♥","♦"];
+  let iSuit = Math.floor(Math.random()*suit.length);
+  return suit[iSuit];
+}
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+window.onload = () => {
+  let selectedSuit = randomSuit();
+
+  let suitTop = document.querySelector(".top.suit");
+  let suitBot = document.querySelector(".bot.suit");
+  
+  suitTop.innerHTML = selectedSuit;
+  suitBot.innerHTML = selectedSuit;
+
+  if(selectedSuit === "♥" || selectedSuit === "♦") {
+    suitTop.classList.add("red");
+    suitBot.classList.add("red");
+  }
+
+  let number = document.querySelector(".number");
+  number.innerHTML = randomNumber();
+}
